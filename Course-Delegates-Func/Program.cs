b252b5276211e97de3cfce16d149d3d853865209
Jesse Fernando <nandopox@gmail.com>
2021-03-19 23:28:07 -0300

    Working with reference method auxiliary

--- Course-Delegates-Func/Program.cs
+++ Course-Delegates-Func/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Course_Delegates_Func.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Course_Delegates_Func
 {
@@ -6,7 +8,24 @@ namespace Course_Delegates_Func
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
     }
 }
